f79e1da10ea03cbb7c098937d7158e66b7bd6486
kaustav <75007246+KaustavDey357@users.noreply.github.com>
2026-09-19 23:59:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,18 @@
 /* config.js: edit these values, no other file needs touching */
-var LEADSEXPO_URL = '';   /* e.g. 'https://leadsexpo.com'. Empty = dock button scrolls to the LeadsExpo section */
-var MEDIUM_URL = '';      /* paste your Medium article or profile URL. Empty = Medium entry hidden */
-var MEDIUM_TITLE = 'My writing on Medium';
+var LEADSEXPO_URL = 'https://leadsexpo.uk';
 var LINKEDIN_URL = 'https://www.linkedin.com/in/kaustav-dey-107593244';
+var MEDIUM_URL = 'https://medium.com/@deykaustav357';
+
+/* Medium posts: one line per article. date is YYYY-MM-DD and is used for sorting. */
+var MEDIUM_POSTS = [
+  { date: '2025-06-11', title: 'Build a serverless image optimizer with AWS Lambda and S3 (free tier safe)', href: 'https://medium.com/@deykaustav357/build-a-serverless-image-optimizer-with-aws-lambda-s3-free-tier-safe-8722817add2f' },
+  { date: '2025-05-15', title: 'How I provisioned scalable AWS infrastructure with Terraform and a load balancer', href: 'https://medium.com/@deykaustav357/how-i-provisioned-scalable-aws-infrastructure-with-terraform-and-load-balancer-published-true-26a2c7c33b47' },
+  { date: '2025-05-15', title: 'How I deployed an app to AWS using CI/CD and Terraform', href: 'https://medium.com/@deykaustav357/how-i-deployed-an-app-to-aws-using-ci-cd-terraform-0b0692dfb97a' },
+  { date: '2025-05-08', title: 'No IDE? Spinning up a cloud VM from the command line', href: 'https://medium.com/@deykaustav357/%EF%B8%8F-no-ide-ff33ca744735' },
+  { date: '2025-05-07', title: 'From S3 to speed: how I supercharged my static website with AWS CloudFront', href: 'https://medium.com/@deykaustav357/from-s3-to-speed-how-i-supercharged-my-static-website-with-aws-cloudfront-8d4b13167751' },
+  { date: '2025-05-06', title: 'How I launched my first EC2 instance on AWS (a beginner\u2019s guide)', href: 'https://medium.com/@deykaustav357/how-i-launched-my-first-ec2-instance-on-aws-a-beginners-guide-56265572cea0' },
+  { date: '2025-05-05', title: 'I made my first AWS Lambda function (and it did absolutely nothing)', href: 'https://medium.com/@deykaustav357/i-made-my-first-aws-lambda-function-and-it-did-absolutely-nothing-0ae006d0a832' },
+  { date: '2025-05-04', title: 'How I built my first VPC with 6 subnets (and only mildly panicked)', href: 'https://medium.com/@deykaustav357/how-i-built-my-first-vpc-with-6-subnets-and-only-mildly-panicked-b68e750c554a' },
+  { date: '2025-04-23', title: 'How I made my first static website using Amazon S3 (as an AWS beginner)', href: 'https://medium.com/@deykaustav357/how-i-made-my-first-static-website-using-amazon-s3-as-an-aws-beginner-476a9232f360' },
+  { date: '2025-04-23', title: 'Diving into the world of AWS: my excitement and journey ahead', href: 'https://medium.com/@deykaustav357/diving-into-the-world-of-aws-my-excitement-and-journey-ahead-13ef3044f646' }
+];
